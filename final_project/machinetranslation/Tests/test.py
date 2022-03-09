7a23b54e3735bce9a4f22c28a01db7af40a4d07b
dgeertsen <devon.geertsen@gmail.com>
2022-03-09 01:59:18 +0000

    Final Changes

---
+++ final_project/machinetranslation/Tests/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from translator import english_to_french, french_to_english
+
+class TestE2F(unittest.TestCase): 
+    def test1(self):
+        self.assertEqual(english_to_french("Hello"), "Bonjour")  # test for the translation of the world 'Hello' and 'Bonjour'.
+
+class TestF2E(unittest.TestCase): 
+    def test1(self): 
+        self.assertEqual(french_to_english("Bonjour"), "Hello")  # test for the translation of the world 'Hello' and 'Bonjour'.
+
+unittest.main()
